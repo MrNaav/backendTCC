cafafb551d6c5d9aed3974b20c363029376e257f
MrNaav <victoranjosbrilhador@gmail.com>
2024-05-27 19:17:28 -0300

    jvbackend

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const oracledb = require('oracledb');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const secretKey = 'skljaksdj9983498327453lsldkjf';
 
 // Configurar o modo Thick
 oracledb.initOracleClient({ libDir: 'C:\\Program Files (x86)\\Oracle\\instantclient_21_13' });
 
+// Inicialização do express
 const app = express();
+
+// Estabeleçendo a porta que rodará o Backend
 const PORT = process.env.PORT || 3000;
+
+// Chave secreta para o Login
+const secretKey = 'skljaksdj9983498327453lsldkjf';
 
 // Habilitar CORS
 app.use(cors());
@@ -65,6 +70,49 @@ app.get('/consultores', async (req, res) => {
             contato: row[2],
             email: row[3],
             senha: row[4]
+        }));
+
+        res.json(consultores);
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao buscar consultores:', error);
+        res.status(500).json({ error: 'Erro ao buscar consultores' });
+    }
+});
+
+// GET consultor por nome
+app.get('/consultores/:nomeConsultor', async (req, res) => {
+    const { nomeConsultor } = req.params;
+
+    if (!nomeConsultor) {
+        return res.status(400).json({ error: 'Nome do consultor é obrigatório' });
+    }
+
+    try {
+        const connection = await oracledb.getConnection(dbConfig);
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const query = `SELECT e.nome as empresa_nome, c.pessoa_id as id_consultor, p.nome as pessoa_nome, p.contato as pessoa_contato, 
+                       p.email as pessoa_email, c.senha as consultor_senha
+                       FROM empresa e 
+                       INNER JOIN consultor c ON e.id = c.empresa_id 
+                       INNER JOIN pessoa p ON c.pessoa_id = p.id
+                       WHERE p.nome = :nomeConsultor`;
+        const result = await connection.execute(query, [nomeConsultor]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Consultor não encontrado' });
+        }
+
+        const consultores = result.rows.map(row => ({
+            empresa_nome: row[0],
+            id_consultor: row[1],
+            nome: row[2],
+            contato: row[3],
+            email: row[4],
+            senha: row[5]
         }));
 
         res.json(consultores);
@@ -247,14 +295,14 @@ app.get('/itensdescarte/:id/localidades', async (req, res) => {
         await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
 
         const query = `SELECT l.id, l.nome, l.horario_funcionamento, e.rua, e.numero, e.complemento, 
-                      b.nome AS bairro, c.nome AS cidade, es.nome AS estado, p.nome AS pais FROM local_descarte l
-                      JOIN endereco e ON l.endereco_id = e.id
-                      JOIN bairro b ON e.bairro_id = b.id
-                      JOIN cidade c ON b.cidade_id = c.id
-                      JOIN estado es ON c.estado_id = es.id
-                      JOIN pais p ON es.pais_id = p.id
-                      JOIN local_item_descarte lid ON l.id = lid.local_descarte_id
-                      WHERE lid.item_descarte_id = :id`;
+                       b.nome AS bairro, c.nome AS cidade, es.nome AS estado, p.nome AS pais FROM local_descarte l
+                       JOIN endereco e ON l.endereco_id = e.id
+                       JOIN bairro b ON e.bairro_id = b.id
+                       JOIN cidade c ON b.cidade_id = c.id
+                       JOIN estado es ON c.estado_id = es.id
+                       JOIN pais p ON es.pais_id = p.id
+                       JOIN local_item_descarte lid ON l.id = lid.local_descarte_id
+                       WHERE lid.item_descarte_id = :id`;
         const result = await connection.execute(query, [id]);
 
         const localidades = result.rows.map(row => ({
@@ -289,9 +337,9 @@ app.get('/contatos_consultoria', async (req, res) => {
         await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
 
         const query = `SELECT cc.id AS contato_id, p.nome AS nome, p.contato AS celular, p.email AS email, cc.motivo AS motivo, cc.detalhes AS detalhes
-                      FROM contato_consultoria cc
-                      INNER JOIN usuario u ON cc.usuario_pessoa_id = u.pessoa_id
-                      INNER JOIN pessoa p ON u.pessoa_id = p.id`;
+                       FROM contato_consultoria cc
+                       INNER JOIN usuario u ON cc.usuario_pessoa_id = u.pessoa_id
+                       INNER JOIN pessoa p ON u.pessoa_id = p.id`;
         const result = await connection.execute(query);
 
         const contatos = result.rows.map(row => ({
@@ -309,6 +357,36 @@ app.get('/contatos_consultoria', async (req, res) => {
     } catch (error) {
         console.error('Erro ao buscar contatos de consultoria:', error);
         res.status(500).json({ error: 'Erro ao buscar contatos de consultoria' });
+    } 
+});
+
+// GET respostas
+app.get('/respostas', async (req, res) => {
+    try {
+        const connection = await oracledb.getConnection(dbConfig);
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const query = `SELECT r.id, r.descricao, r.contato_consultoria_id, r.consultor_pessoa_id, p.nome AS consultor_nome
+                       FROM resposta r
+                       INNER JOIN consultor c ON r.consultor_pessoa_id = c.pessoa_id
+                       INNER JOIN pessoa p ON c.pessoa_id = p.id`;
+        const result = await connection.execute(query);
+
+        const respostas = result.rows.map(row => ({
+            id: row[0],
+            descricao: row[1],
+            contato_consultoria_id: row[2],
+            consultor_pessoa_id: row[3],
+            consultor_nome: row[4]
+        }));
+
+        res.json(respostas);
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao buscar respostas:', error);
+        res.status(500).json({ error: 'Erro ao buscar respostas' });
     } 
 });
 
@@ -365,14 +443,12 @@ app.post('/item_descarte', async (req, res) => {
 
         await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
 
-        const insertItemQuery = `INSERT INTO item_descarte (nome, descricao, riscos, empresa_id) VALUES (:nome, :descricao, :riscos, 1) RETURNING id INTO :id`;
-        const itemResult = await connection.execute(insertItemQuery, { nome, descricao, riscos, id: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT } });
-
-        const itemId = itemResult.outBinds.id[0];
+        const insertItemQuery = `INSERT INTO item_descarte (nome, descricao, riscos, empresa_id) VALUES (:nome, :descricao, :riscos, 1)`;
+        await connection.execute(insertItemQuery, { nome, descricao, riscos });
 
         await connection.execute(`COMMIT`);
 
-        res.status(201).json({ message: 'Item de descarte cadastrado com sucesso!', id: itemId });
+        res.status(201).json({ message: 'Item de descarte cadastrado com sucesso!'});
 
         await connection.close();
     } catch (error) {
@@ -531,7 +607,7 @@ app.post('/resposta_contato', async (req, res) => {
         await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
 
         const insertRespostaQuery = `INSERT INTO resposta (descricao, contato_consultoria_id, consultor_pessoa_id) 
-                                    VALUES (:resposta, :contato_consultoria_id, :consultor_id) `;
+                                     VALUES (:resposta, :contato_consultoria_id, :consultor_id) `;
         await connection.execute( insertRespostaQuery , { resposta, contato_consultoria_id, consultor_id });
 
         await connection.execute(`COMMIT`);
@@ -554,12 +630,171 @@ app.post('/resposta_contato', async (req, res) => {
     } 
 });
 
-// POST login
-app.post('/login', async (req, res) => {
-    const { email, senha } = req.body;
+// PUT item_descarte
+app.put('/item_descarte/:id', async (req, res) => {
+    const connection = await oracledb.getConnection(dbConfig);
 
     try {
+        const { id } = req.params;
+        const { nome, descricao, riscos } = req.body;
+        const { locais } = req.body;
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const updateItemQuery = `UPDATE item_descarte SET nome = :nome, descricao = :descricao, riscos = :riscos WHERE id = :id`;
+        await connection.execute(updateItemQuery, { nome, descricao, riscos, id });
+
+        const deleteLocaisQuery = `DELETE FROM local_item_descarte WHERE ITEM_DESCARTE_ID = :id`;
+        await connection.execute(deleteLocaisQuery, [id]);
+
+        const addLocaisQuery = `INSERT INTO local_item_descarte (ITEM_DESCARTE_ID, LOCAL_DESCARTE_ID) VALUES (:idItemDescarte, :idLocalDescarte)`;
+        if (locais) {
+            for (const localId of locais) {
+                await connection.execute(addLocaisQuery, [id, localId]);
+            }
+        }
+
+        await connection.execute(`COMMIT`);
+
+        res.status(200).json({ message: 'Item de descarte atualizado com sucesso!' });
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao atualizar item de descarte:', error);
+
+        if (connection) {
+            try {
+                await connection.execute(`ROLLBACK`);
+            } catch (rollbackError) {
+                console.error('Erro ao executar rollback:', rollbackError);
+            }
+        }
+
+        res.status(500).json({ error: 'Erro ao atualizar item de descarte' });
+    }
+});
+
+// DELETE item_descarte
+app.delete('/item_descarte/:id', async (req, res) => {
+    const connection = await oracledb.getConnection(dbConfig);
+
+    try {
+        const { id } = req.params;
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const checkAssociationQuery = `SELECT COUNT(*) AS total FROM local_item_descarte WHERE item_descarte_id = :id`;
+        const associationResult = await connection.execute(checkAssociationQuery, { id });
+        const totalAssociations = associationResult.rows[0][0];
+
+        if (totalAssociations > 0) {
+            const deleteAssociationQuery = `DELETE FROM local_item_descarte WHERE item_descarte_id = :id`;
+            await connection.execute(deleteAssociationQuery, { id });
+        }
+
+        const deleteItemQuery = `DELETE FROM item_descarte WHERE id = :id`;
+        await connection.execute(deleteItemQuery, { id });
+
+        await connection.execute(`COMMIT`);
+
+        res.status(200).json({ message: 'Item de descarte deletado com sucesso!' });
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao deletar item de descarte:', error);
+
+        if (connection) {
+            try {
+                await connection.execute(`ROLLBACK`);
+            } catch (rollbackError) {
+                console.error('Erro ao executar rollback:', rollbackError);
+            }
+        }
+
+        res.status(500).json({ error: 'Erro ao deletar item de descarte' });
+    }
+});
+
+// DELETE local_descarte
+app.delete('/local_descarte/:id', async (req, res) => {
+    const connection = await oracledb.getConnection(dbConfig);
+
+    try {
+        const { id } = req.params;
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const checkAssociationQuery = `SELECT COUNT(*) AS total FROM local_item_descarte WHERE local_descarte_id = :id`;
+        const associationResult = await connection.execute(checkAssociationQuery, { id });
+        const totalAssociations = associationResult.rows[0][0];
+
+        if (totalAssociations > 0) {
+            return res.status(400).json({ error: 'Este local de descarte possui itens associados e não pode ser excluído.' });
+        }
+
+        const deleteLocalQuery = `DELETE FROM local_descarte WHERE id = :id`;
+        await connection.execute(deleteLocalQuery, { id });
+
+        await connection.execute(`COMMIT`);
+
+        res.status(200).json({ message: 'Local de descarte excluído com sucesso!' });
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao excluir local de descarte:', error);
+
+        if (connection) {
+            try {
+                await connection.execute(`ROLLBACK`);
+            } catch (rollbackError) {
+                console.error('Erro ao executar rollback:', rollbackError);
+            }
+        }
+
+        res.status(500).json({ error: 'Erro ao excluir local de descarte' });
+    }
+});
+
+// DELETE local_item_descarte
+app.delete('/local_item_descarte/:idItemDescarte/:idLocalDescarte', async (req, res) => {
+    const connection = await oracledb.getConnection(dbConfig);
+
+    try {
+        const { idItemDescarte, idLocalDescarte } = req.params;
+
+        await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
+
+        const deleteLocalItemDescarteQuery = `DELETE FROM local_item_descarte 
+                                              WHERE item_descarte_id = :idItemDescarte AND local_descarte_id = :idLocalDescarte`;
+        await connection.execute(deleteLocalItemDescarteQuery, { idItemDescarte, idLocalDescarte });
+
+        await connection.execute(`COMMIT`);
+
+        res.status(200).json({ message: 'Associação entre item de descarte e local de descarte excluída com sucesso!' });
+
+        await connection.close();
+    } catch (error) {
+        console.error('Erro ao excluir associação entre item de descarte e local de descarte:', error);
+
+        if (connection) {
+            try {
+                await connection.execute(`ROLLBACK`);
+            } catch (rollbackError) {
+                console.error('Erro ao executar rollback:', rollbackError);
+            }
+        }
+
+        res.status(500).json({ error: 'Erro ao excluir associação entre item de descarte e local de descarte' });
+    }
+});
+
+// POST login
+app.post('/login', async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        
         const connection = await oracledb.getConnection(dbConfig);
+
         await connection.execute(`ALTER SESSION SET CURRENT_SCHEMA = jvSolucoess`);
 
         const query = `SELECT p.email, c.senha FROM consultor c INNER JOIN pessoa p ON c.pessoa_id = p.id WHERE p.email = :email AND c.senha = :senha`;
